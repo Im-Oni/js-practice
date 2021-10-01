29582ae5e75fa87d48eb7b5ad666f730351bb3cd
Im-Oni <konstantinmakarenko99@gmail.com>
2021-10-01 18:35:36 +0300

    for

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-
-const numertOfFilms = +prompt('Сколько фильмов вы посмотрели?', ``);
+const numertOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
 
 let personalMovieDB = {
     
@@ -10,13 +9,44 @@ let personalMovieDB = {
     privat: false,
 };
 
-const lastMovie = prompt('Один из последних просмотренных фильмов?', '');
-const gradeMovie = prompt('На сколько оцените его?', '');
-const lastMovie2 = prompt('Один из последних просмотренных фильмов?', '');
-const gradeMovie2 = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[lastMovie] = gradeMovie;
-personalMovieDB.movies[lastMovie2] = gradeMovie2;
+
+
+if(personalMovieDB.count< 10) {
+
+    alert('Просмотрено довольно мало фильмов');
+
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+
+    alert('Вы классический зритель');
+
+} else if (personalMovieDB.count >= 30){
+    alert('Вы Киноман!!!');
+} else {
+    alert('Произошла Ошибочка');
+}
+
+
+
+for(let i = 0; i<2; i++){
+
+    let lastMovie = prompt('Один из последних просмотренных фильмов?', '');
+    let gradeMovie = prompt('На сколько оцените его?', '');
+
+    if(lastMovie != null && gradeMovie != null &&
+       lastMovie != '' && gradeMovie != '' &&
+       lastMovie.length <50){
+
+        personalMovieDB.movies[lastMovie] = gradeMovie;
+
+    } else {
+
+        i--;
+
+    }
+
+}
+
 
 console.log(personalMovieDB);
 
